076b21fdb9da5a79dee09a42014fd5691486243c
bsnernier <nbern817@gmail.com>
2021-06-02 10:29:16 -0500

    changed some variables from plural to singular

--- db/models/follower.js
+++ db/models/follower.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Follower = sequelize.define('Follower', {
     userId: DataTypes.INTEGER,
-    followersId: DataTypes.INTEGER
+    followerId: DataTypes.INTEGER
   }, {});
   Follower.associate = function(models) {
     // associations can be defined here
--- db/models/like.js
+++ db/models/like.js
@@ -3,12 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
     count: DataTypes.INTEGER,
     userId: DataTypes.INTEGER,
-    storiesId: DataTypes.INTEGER,
+    storyId: DataTypes.INTEGER,
     commentId: DataTypes.INTEGER
   }, {});
   Like.associate = function(models) {
     // associations can be defined here
-    Like.belongsTo(models.Story, {foreignKey: 'storiesId'}),
+    Like.belongsTo(models.Story, {foreignKey: 'storyId'}),
     Like.belongsTo(models.Comment, {foreignKey: 'commentId'}),
     Like.belongsTo(models.User, {foreignKey: 'userId'})
   };
