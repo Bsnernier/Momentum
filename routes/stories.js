eb1ee563fc826ff2c31c9ddb7d8f110a24d9de70
Jia Cui <cuijiajanae@gmail.com>
2021-06-01 20:27:05 -0700

    finished stories pug/css and layout pug/css

--- routes/stories.js
+++ routes/stories.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const commentsRouter = require('../routes/comments');
 const likesRouter = require('../routes/likes');
-const {asyncHandler, handleValidationErrors, cookieParser, csrfProtection} = require('../../utils');
+const {asyncHandler, handleValidationErrors, cookieParser, csrfProtection} = require('../utils.js');
 
 // router.use('/comments', commentsRouter);
 // router.use('/likes', likesRouter);
@@ -11,3 +11,10 @@ const {asyncHandler, handleValidationErrors, cookieParser, csrfProtection} = req
 router.get("/", asyncHandler(async(req, res)=>{
     res.render("stories")
 }))
+
+router.post("/",asyncHandler(async(req, res)=>{
+    res.render("post")
+}))
+
+
+module.exports = router;
