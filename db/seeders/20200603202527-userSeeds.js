4c155616644a8e72f1774f37a53c5dd9b7d5299f
Matt Melendez <matthewemelendez@gmail.com>
2021-06-04 21:30:29 -0700

    10 PM seeder/schema work

--- db/seeders/20200603202527-userSeeds.js
+++ db/seeders/20200603202527-userSeeds.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
-    {username:  'mattmelendez', firstName: 'matt', lastName:' melendez', email:' mattmelendez@gmail.com' , password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()  } ,
+    {username:  'mattmelendez', firstName: 'matt', lastName:' melendez', email: 'mattmelendez@gmail.com' , password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()  } ,
     {username: 'natebernier', firstName: 'nate', lastName: 'bernier' , email: 'natebernier@gmail.com' , password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()} ,
     {username: 'jiacui', firstName: 'jia', lastName: 'cui' , email: 'jiacui@gmail.com' , password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()} ,
     {username: 'edherman', firstName: 'ed', lastName: 'herman' , email: 'edherman@gmail.com' ,  password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
