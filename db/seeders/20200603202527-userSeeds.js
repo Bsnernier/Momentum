a690f1764f2c8ee7a4527af706855a7e75b95836
Matt Melendez <matthewemelendez@gmail.com>
2021-06-04 22:10:01 -0700

    Followers, Users, and Stories seed files complete

--- db/seeders/20200603202527-userSeeds.js
+++ db/seeders/20200603202527-userSeeds.js
@@ -4,12 +4,16 @@ const bcrypt = require('bcryptjs');
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
-    {username:  'mattmelendez', firstName: 'matt', lastName:' melendez', email: 'mattmelendez@gmail.com' , password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()  } ,
-    {username: 'natebernier', firstName: 'nate', lastName: 'bernier' , email: 'natebernier@gmail.com' , password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()} ,
-    {username: 'jiacui', firstName: 'jia', lastName: 'cui' , email: 'jiacui@gmail.com' , password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()} ,
-    {username: 'edherman', firstName: 'ed', lastName: 'herman' , email: 'edherman@gmail.com' ,  password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
-    {username: 'rihanamartinson', firstName: 'rihana', lastName: 'martinson' , email: 'rihanamartinson@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()}
-    ], {});
+      {username: 'troybarnes', firstName: 'Troy', lastName: 'Barnes', email: 'troybarnes@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
+      {username: 'jeffwinger', firstName: 'Jeff', lastName:' Winger', email: 'jeffwinger@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
+      {username: 'annieedison', firstName: 'Annie', lastName: 'Edison', email: 'annieedison@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
+      {username: 'brittaperry', firstName: 'Britta', lastName: 'Perry', email: 'brittaperry@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
+      {username: 'abednadir', firstName: 'Abed', lastName: 'Nadir', email: 'abednadir@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
+      {username: 'shirleybennett', firstName: 'Shirley', lastName: 'Bennett', email: 'shirleybennett@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
+      {username: 'piercehawthorne', firstName: 'Pierce', lastName: 'Hawthorne', email: 'piercehawthorne@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
+      {username: 'craigpelton', firstName: 'Craig', lastName: 'Pelton', email: 'craigpelton@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()},
+      {username: 'benchang', firstName: 'Ben', lastName: 'Chang', email: 'benchang@gmail.com', password: bcrypt.hashSync('Ab1!'), createdAt: new Date(), updatedAt: new Date()}]
+      , {});
   },
 
   down: (queryInterface, Sequelize) => {
