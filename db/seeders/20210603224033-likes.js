06937026beccb6590f32d3a2146836e9b4948ec4
Nate Bernier <78568625+Bsnernier@users.noreply.github.com>
2021-06-04 20:51:42 -0500

    Update 20210603224033-likes.js

--- db/seeders/20210603224033-likes.js
+++ db/seeders/20210603224033-likes.js
@@ -3,10 +3,10 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('Likes', [
-        { count: 33, userId: 1, storyId: 1, createdAt: new Date(), updatedAt: new Date() },
-        { count: 33, userId: 2, storyId: 1, createdAt: new Date(), updatedAt: new Date() },
-        { count: 33, userId: 1, storyId: 2, createdAt: new Date(), updatedAt: new Date() },
-        { count: 33, userId: 1, storyId: 3, createdAt: new Date(), updatedAt: new Date() },
+        { userId: 1, storyId: 1, createdAt: new Date(), updatedAt: new Date() },
+        { userId: 2, storyId: 1, createdAt: new Date(), updatedAt: new Date() },
+        { userId: 1, storyId: 2, createdAt: new Date(), updatedAt: new Date() },
+        { userId: 1, storyId: 3, createdAt: new Date(), updatedAt: new Date() },
       ], {});
   },
 
