2c8a2a6538c1113a235dfa2dbe7af77ba7d52ff9
Jia Cui <cuijiajanae@gmail.com>
2021-06-04 18:20:53 -0700

    doing some edit stuff

--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {asyncHandler, handleValidationErrors, cookieParser, csrfProtection} = require('../utils');
+const db = require('../db/models');
+const { User, Comment, Story } = db;
+const { requireAuth } = require('../auth')
 
+
+router.get("/post/new/:id", asyncHandler(async(req, res)=>{
+
+
+    
+
+    res.render("postPrefilled", {content, image, location})
+}))
+
+
+
+module.exports = router;
