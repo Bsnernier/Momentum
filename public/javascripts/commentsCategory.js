e5af28b64e97299df1c02f455cc18a3167d1cd42
bsnernier <nbern817@gmail.com>
2021-06-06 15:41:18 -0500

    Final Test 4 for Heroku

--- public/javascripts/commentsCategory.js
+++ public/javascripts/commentsCategory.js
@@ -52,7 +52,7 @@ window.addEventListener("DOMContentLoaded", (event)=>{
         const id = event.target.id
         console.log("my id: ", id)
 
-        const res = await fetch(`/api/comments/category/${id}`, {
+        const res = await fetch(`/api/comments/${id}`, {
           method: "POST",
           body: JSON.stringify(body),
           headers: {
--- routes/apiRoutes/comments.js
+++ routes/apiRoutes/comments.js
@@ -36,27 +36,27 @@ const validateComment = [
     handleValidationErrors,
 ];
 
-router.post(
-    "/category/:id",
-    validateComment,
-    jsonParser,
-    requireAuth,
-    asyncHandler(async (req, res) => {
-      const { content } = req.body;
+// router.post(
+//     "/category/:id",
+//     validateComment,
+//     jsonParser,
+//     requireAuth,
+//     asyncHandler(async (req, res) => {
+//       const { content } = req.body;
 
-      try{
-        const id = req.params.id
-        const { userId } = req.session.auth;
-        const story = await Story.findByPk(id)
-        const {category} = story;
-        const comments = await Comment.create({ content, userId: userId, storyId:id });
-        res.json({comments, category})
-      }catch(e){
-        console.log("Error in posting comment");
-        console.log(e);
-      }
-    })
-);
+//       try{
+//         const id = req.params.id
+//         const { userId } = req.session.auth;
+//         const story = await Story.findByPk(id)
+//         const {category} = story;
+//         const comments = await Comment.create({ content, userId: userId, storyId:id });
+//         res.json({comments, category})
+//       }catch(e){
+//         console.log("Error in posting comment");
+//         console.log(e);
+//       }
+//     })
+// );
 
 router.post(
     "/:id",
@@ -80,27 +80,27 @@ router.post(
     })
 );
 
-router.post(
-    "/personal/:id",
-    validateComment,
-    jsonParser,
-    requireAuth,
-    asyncHandler(async (req, res) => {
-      const { content } = req.body;
+// router.post(
+//     "/personal/:id",
+//     validateComment,
+//     jsonParser,
+//     requireAuth,
+//     asyncHandler(async (req, res) => {
+//       const { content } = req.body;
 
-      try{
-        const id = req.params.id
-        const { userId } = req.session.auth;
-        const story = await Story.findByPk(id)
-        const {category} = story;
-        const comments = await Comment.create({ content, userId: userId, storyId:id });
-        res.json({comments, category})
-      }catch(e){
-        console.log("Error in posting comment");
-        console.log(e);
-      }
-    })
-);
+//       try{
+//         const id = req.params.id
+//         const { userId } = req.session.auth;
+//         const story = await Story.findByPk(id)
+//         const {category} = story;
+//         const comments = await Comment.create({ content, userId: userId, storyId:id });
+//         res.json({comments, category})
+//       }catch(e){
+//         console.log("Error in posting comment");
+//         console.log(e);
+//       }
+//     })
+// );
 
 
 
