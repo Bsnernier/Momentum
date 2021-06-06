7efdb75c4fd1809f0a3ee351ec729b97a2679720
Jia Cui <cuijiajanae@gmail.com>
2021-06-06 08:07:37 -0700

    really ready to push and testnpm startnpm test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { requireAuth } = require("../auth");
 
 /* GET home page. */
-router.get('/', requireAuth, function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', {title1: "MOMENTUM"});
 });
 
