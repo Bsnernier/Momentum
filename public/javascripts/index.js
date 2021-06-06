7527ea3bd3e8e3c0a7d2d0c26b2e805ea91750cd
bsnernier <nbern817@gmail.com>
2021-06-06 17:37:20 -0500

    comment out public/index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,11 @@
-window.addEventListener("DOMContentLoaded", (event)=>{
-    const fetchStories = async () => {
-        const res = await fetch('/api/stories');
-        if (res.status === 401) {
-          window.location.href = "/log-in";
-          return;
-        }
-      };
+// window.addEventListener("DOMContentLoaded", (event)=>{
+//     const fetchStories = async () => {
+//         const res = await fetch('/api/stories');
+//         if (res.status === 401) {
+//           window.location.href = "/log-in";
+//           return;
+//         }
+//       };
 
-      fetchStories();
-})
+//       fetchStories();
+// })
